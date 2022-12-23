80fccfef56107567986fc14dab2d1dc88a8b158f
Catalin B <catalin.bac@gmail.com>
2022-12-23 11:39:14 +0200

    some changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,32 +1,95 @@
 let layoutHandler;
 
-(function($) {
-    'use strict';
-    let LayoutHandler = function() {
-        let self = this;
+(function ($) {
+  'use strict';
+  let LayoutHandler = function () {
+    let self = this;
 
-        this.ready = function() {
-            this.handleDOM();
-            this.handleEvents();
-        };
-
-        this.handleDOM = function() {
-
-        };
-
-        this.handleEvents = function() {
-
-        };
+    this.ready = function () {
+      this.handleDOM();
+      this.handleEvents();
     };
 
-    layoutHandler = new LayoutHandler();
+    this.handleDOM = function () {
 
-    $(document).ready(function() {
-        layoutHandler.ready();
+    };
+
+    this.handleEvents = function () {
+
+    };
+  };
+
+  layoutHandler = new LayoutHandler();
+
+  $(document).ready(function () {
+    layoutHandler.ready();
 
 
+
+
+
+
+
+
+
+
+    const getHamburber = document.querySelector('.hamburger');
+    const getNav = document.querySelector('.main-ul');
+    getHamburber.addEventListener('click', () => {
+      getNav.classList.toggle('active');
     });
+
+
+
+
+    var swiper = new Swiper(".main", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1.3,
+          spaceBetween: 15,
+        },
+        520: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+        // 1400: {
+        //   slidesPerView: 3,
+        // },
+      },
+    });
+
+
+  });
 })(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* eslint-disable linebreak-style */
 (function() {
@@ -34,34 +97,13 @@ let layoutHandler;
   // Your custom JavaScript goes here
 
 
-
-
 // creative code
 
-const getHamburber = document.querySelector('.hamburger');
-const getNav = document.querySelector('.main-ul');
-getHamburber.addEventListener('click', () =>{
-  getNav.classList.toggle('active');
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const getHamburber = document.querySelector('.hamburger');
+// const getNav = document.querySelector('.main-ul');
+// getHamburber.addEventListener('click', () =>{
+//   getNav.classList.toggle('active');
+// });
 })();
-
 
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,34 +4,13 @@
   // Your custom JavaScript goes here
 
 
-
-
 // creative code
 
-const getHamburber = document.querySelector('.hamburger');
-const getNav = document.querySelector('.main-ul');
-getHamburber.addEventListener('click', () =>{
-  getNav.classList.toggle('active');
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const getHamburber = document.querySelector('.hamburger');
+// const getNav = document.querySelector('.main-ul');
+// getHamburber.addEventListener('click', () =>{
+//   getNav.classList.toggle('active');
+// });
 })();
-
 
 
